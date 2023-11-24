e938716f0c14638c3b425ea8445b8bef9019dc37
Marcus Vinicius Oliveira Silva <marcusvi200@gmail.com>
2023-11-24 10:40:15 -0300

    fixed: added missing functions

--- xml2js.d.ts
+++ xml2js.d.ts
@@ -1,7 +1,9 @@
 declare namespace xml2js {
+    export function Parser(options?: any): any;
     export function parseString(xml: string, options: any, callback: (err: any, result: any) => void): void;
     export function parseString(xml: string, callback: (err: any, result: any) => void): void;
     export function parseString(xml: string, options?: any): Promise<any>;
+    export function parseStringPromise(xml: string, options?: any): Promise<any>;
     export function Builder(options?: any): any;
 }
 
